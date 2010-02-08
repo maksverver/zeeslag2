ebc84755c41303c001b95929179e1dd5e4b6b229
Maks Verver <maksverver@geocities.com>
2010-02-09 00:58:51 +0100

    Finished solver:  - takes command line arguments  - takes existing shots (hits and misses) into account  - prints a random optimal move (using greedy heuristics)  - optionally prints detailed information about position (for verification)

--- solver.c
+++ solver.c
@@ -4,29 +4,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <time.h>
 
-#define SHIPS 10
-static const int lengths[SHIPS] = { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 };
+#define HEIGHT       16         /* field height */
+#define WIDTH        16         /* field width */
+#define SHIPS        10         /* number of ships */
+#define SHIP_PARTS   30         /* sum of ship lengths */
 
-#define MAX_HITS 30
-static int g_hits[MAX_HITS];
-static int g_num_hits = 0;
+static const int lengths[SHIPS] = { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2  };
+
+struct Coords { int r, c; };
 
 struct SolverState {
-    int rows[16], cols[16];
-    char blocked[16][16];
-    int hits[16][16];
+    int rows[HEIGHT], cols[WIDTH];      /* current per-row/column ship counts */
+    char blocked[HEIGHT][WIDTH];        /* cells blocked from placing ships */
+    char required[HEIGHT][WIDTH];       /* cells on which a ship must be placed */
+    int required_left;                  /* number of required cells left */
+    int hit_counts[HEIGHT][WIDTH];      /* resulting per-cell hit counts */
 };
 
 struct ThreadState {
-    struct SolverState ss;
-    int r1, c1, dir;
+    struct SolverState ss;              /* thread-local solver state */
+    int r1, c1, dir;                    /* row, col, dir of first ship */
 };
 
 static inline int min(int a, int b)  { return a < b ? a : b; }
 static inline int max(int a, int b)  { return a > b ? a : b; }
 
-static void count_hits(int hits[16][16], int r1, int c1, int r2, int c2, int cnt)
+static void record_hits(int hits[HEIGHT][WIDTH], int r1, int c1, int r2, int c2, int cnt)
 {
     for (int r = r1; r < r2; ++r) {
         for (int c = c1; c < c2; ++c) {
@@ -35,18 +41,20 @@ static void count_hits(int hits[16][16], int r1, int c1, int r2, int c2, int cnt
     }
 }
 
-static inline bool check_counts(int counts[16], int a, int b)
+static inline bool check_counts(int *counts, int size, int a, int b)
 {
     for (int i = a; i < b; ++i) {
-        if (counts[i] == 1 && (i == 0 || counts[i - 1] == 0) && (i + 1 == 16 || counts[i + 1] == 0)) return false;
+        if ( counts[i] == 1 &&
+             (i - 1 <     0 || counts[i - 1] == 0) &&
+             (i + 1 >= size || counts[i + 1] == 0) ) return false;
     }
     return true;
 }
 
-int place(struct SolverState *ss, int ship, int last_r, int last_c)
+static int place(struct SolverState *ss, int ship, int last_r, int last_c)
 {
     if (ship == SHIPS) {
-        return 1;
+        return ss->required_left == 0 ? 1 : 0;
     }
 
     const int length = lengths[ship];
@@ -57,31 +65,33 @@ int place(struct SolverState *ss, int ship, int last_r, int last_c)
     int res = 0;
 
     /* Place horizontally: */
-    for (int r = last_r; r < 16 ; ++r) {
+    for (int r = last_r; r < HEIGHT ; ++r) {
         if (ss->rows[r] < length) continue;
-        for (int c1 = r == last_r ? last_c : 0; c1 <= 16 - length; ++c1) {
+        for (int c1 = r == last_r ? last_c : 0; c1 <= WIDTH - length; ++c1) {
             const int c2 = c1 + length;
             for (int c = c1; c < c2; ++c) {
                 if (ss->cols[c] == 0 || ss->blocked[r][c]) goto h_next;
             }
-            const int bc1 = max(c1 - 1,  0), bc2 = min(c2 + 1, 16);
+            const int bc1 = max(c1 - 1, 0), bc2 = min(c2 + 1, WIDTH);
             ss->rows[r] -= length;
-            if (!(ss->rows[r] == 1 && (r == 0 || ss->rows[r - 1] == 0) && (r + 1 == 16 || ss->rows[r + 1] == 0)))
-            {
+            if (!( ss->rows[r] == 1 &&
+                   (r     == 0      || ss->rows[r - 1] == 0) &&
+                   (r + 1 == HEIGHT || ss->rows[r + 1] == 0) )) {
                 for (int c = c1; c < c2; ++c) --ss->cols[c];
-                if (check_counts(ss->cols, bc1, bc2))
-                {
-                    if (r - 1 >= 0) for (int c = bc1; c < bc2; ++c) ++ss->blocked[r - 1][c];
-                    if (true)       for (int c = bc1; c < bc2; ++c) ++ss->blocked[r    ][c];
-                    if (r + 1 < 16) for (int c = bc1; c < bc2; ++c) ++ss->blocked[r + 1][c];
+                if (check_counts(ss->cols, WIDTH, bc1, bc2)) {
+                    if (r - 1 >= 0)     for (int c = bc1; c < bc2; ++c) ++ss->blocked[r - 1][c];
+                    if (true)           for (int c = bc1; c < bc2; ++c) ++ss->blocked[r    ][c];
+                    if (r + 1 < HEIGHT) for (int c = bc1; c < bc2; ++c) ++ss->blocked[r + 1][c];
+                    for (int c = c1; c < c2; ++c) if (ss->required[r][c]++) --ss->required_left;
 
                     const int cnt = place(ss, ship + 1, r, bc2);
-                    if (cnt > 0) count_hits(ss->hits, r, c1, r + 1, c2, cnt);
+                    if (cnt > 0) record_hits(ss->hit_counts, r, c1, r + 1, c2, cnt);
                     res += cnt;
 
-                    if (r - 1 >= 0) for (int c = bc1; c < bc2; ++c) --ss->blocked[r - 1][c];
-                    if (true)       for (int c = bc1; c < bc2; ++c) --ss->blocked[r    ][c];
-                    if (r + 1 < 16) for (int c = bc1; c < bc2; ++c) --ss->blocked[r + 1][c];
+                    for (int c = c1; c < c2; ++c) if (--ss->required[r][c]) ++ss->required_left;
+                    if (r - 1 >= 0)     for (int c = bc1; c < bc2; ++c) --ss->blocked[r - 1][c];
+                    if (true)           for (int c = bc1; c < bc2; ++c) --ss->blocked[r    ][c];
+                    if (r + 1 < HEIGHT) for (int c = bc1; c < bc2; ++c) --ss->blocked[r + 1][c];
                 }
                 for (int c = c1; c < c2; ++c) ++ss->cols[c];
             }
@@ -91,35 +101,37 @@ int place(struct SolverState *ss, int ship, int last_r, int last_c)
     }
 
     /* Place vertically: */
-    for (int r1 = last_r; r1 <= 16 - length; ++r1) {
+    for (int r1 = last_r; r1 <= HEIGHT - length; ++r1) {
         const int r2 = r1 + length;
         for (int r = r1; r < r2; ++r) {
             if (ss->rows[r] == 0) goto v_next_1;
         }
-        const int br1 = max(r1 - 1,  0), br2 = min(r2 + 1, 16);
-        for (int c = r1 == last_r ? last_c : 0; c < 16; ++c) {
+        const int br1 = max(r1 - 1,  0), br2 = min(r2 + 1, HEIGHT);
+        for (int c = r1 == last_r ? last_c : 0; c < WIDTH; ++c) {
             if (ss->cols[c] < length) continue;
             for (int r = r1; r < r2; ++r) {
                 if (ss->blocked[r][c]) goto v_next_2;
             }
 
             ss->cols[c] -= length;
-            if (!(ss->cols[c] == 1 && (c == 0 || ss->cols[c - 1] == 0) && (c + 1 == 16 || ss->cols[c + 1] == 0)))
-            {
+            if (!( ss->cols[c] == 1 &&
+                   (c == 0         || ss->cols[c - 1] == 0) &&
+                   (c + 1 == WIDTH || ss->cols[c + 1] == 0)) ) {
                 for (int r = r1; r < r2; ++r) --ss->rows[r];
-                if (check_counts(ss->rows, br1, br2))
-                {
-                    if (c - 1 >= 0) for (int r = br1; r < br2; ++r) ++ss->blocked[r][c - 1];
-                    if (true)       for (int r = br1; r < br2; ++r) ++ss->blocked[r][c    ];
-                    if (c + 1 < 16) for (int r = br1; r < br2; ++r) ++ss->blocked[r][c + 1];
+                if (check_counts(ss->rows, HEIGHT, br1, br2)) {
+                    if (c - 1 >= 0)    for (int r = br1; r < br2; ++r) ++ss->blocked[r][c - 1];
+                    if (true)          for (int r = br1; r < br2; ++r) ++ss->blocked[r][c];
+                    if (c + 1 < WIDTH) for (int r = br1; r < br2; ++r) ++ss->blocked[r][c + 1];
+                    for (int r = r1; r < r2; ++r) if (ss->required[r][c]++) --ss->required_left;
 
-                    const int cnt = place(ss, ship + 1, r1, min(c + 2, 16));
-                    if (cnt > 0) count_hits(ss->hits, r1, c, r2, c + 1, cnt);
+                    const int cnt = place(ss, ship + 1, r1, min(c + 2, WIDTH));
+                    if (cnt > 0) record_hits(ss->hit_counts, r1, c, r2, c + 1, cnt);
                     res += cnt;
 
-                    if (c - 1 >= 0) for (int r = br1; r < br2; ++r) --ss->blocked[r][c - 1];
-                    if (true)       for (int r = br1; r < br2; ++r) --ss->blocked[r][c    ];
-                    if (c + 1 < 16) for (int r = br1; r < br2; ++r) --ss->blocked[r][c + 1];
+                    for (int r = r1; r < r2; ++r) if (--ss->required[r][c]) ++ss->required_left;
+                    if (c - 1 >= 0)    for (int r = br1; r < br2; ++r) --ss->blocked[r][c - 1];
+                    if (true)          for (int r = br1; r < br2; ++r) --ss->blocked[r][c];
+                    if (c + 1 < WIDTH) for (int r = br1; r < br2; ++r) --ss->blocked[r][c + 1];
                 }
                 for (int r = r1; r < r2; ++r) ++ss->rows[r];
             }
@@ -131,9 +143,16 @@ int place(struct SolverState *ss, int ship, int last_r, int last_c)
 
     return res;
 }
-
-void *place_first_thread(void *arg)
+__attribute__((__noreturn__))
+static void fatal(const char *message)
 {
+    printf("ERROR: %s\n", message);
+    exit(EXIT_FAILURE);  /* success because we produced a line of output */
+}
+
+static void *place_first_thread(void *arg)
+{
+    int cnt = 0;
     struct ThreadState *ts = (struct ThreadState *)arg;
     struct SolverState *ss = &ts->ss;
     const int r1 = ts->r1, c1 = ts->c1;
@@ -142,72 +161,88 @@ void *place_first_thread(void *arg)
     const int r2 = r1 + height, c2 = c1 + width;
     for (int r = r1; r < r2; ++r) ss->rows[r] -= width;
     for (int c = c1; c < c2; ++c) ss->cols[c] -= height;
-    const int br1 = max(r1 - 1,  0), bc1 = max(c1 - 1,  0);
-    const int br2 = min(r2 + 1, 16), bc2 = min(c2 + 1, 16);
-    if (!check_counts(ss->rows, br1, br2)) return 0;
-    if (!check_counts(ss->cols, bc1, bc2)) return 0;
-    for (int r = br1; r < br2; ++r) {
-        for (int c = bc1; c < bc2; ++c) ++ss->blocked[r][c];
-    }
-    const int cnt = place(ss, 1, r1, bc2);
-    if (cnt > 0) count_hits(ss->hits, r1, c1, r2, c2, cnt);
-    for (int r = br1; r < br2; ++r) {
-        for (int c = bc1; c < bc2; ++c) --ss->blocked[r][c];
+    const int br1 = max(r1 - 1, 0), br2 = min(r2 + 1, HEIGHT);
+    const int bc1 = max(c1 - 1, 0), bc2 = min(c2 + 1, WIDTH);
+    if ( check_counts(ss->rows, HEIGHT, br1, br2) &&
+         check_counts(ss->cols, WIDTH, bc1, bc2) ) {
+        for (int r = br1; r < br2; ++r) {
+            for (int c = bc1; c < bc2; ++c) ++ss->blocked[r][c];
+        }
+        for (int r = r1; r < r2; ++r) {
+            for (int c = c1; c < c2; ++c) {
+                if (ss->required[r][c]++) --ss->required_left;
+            }
+        }
+        cnt += place(ss, 1, r1, bc2);
+        if (cnt > 0) record_hits(ss->hit_counts, r1, c1, r2, c2, cnt);
+        for (int r = r1; r < r2; ++r) {
+            for (int c = c1; c < c2; ++c) {
+                if (--ss->required[r][c]) ++ss->required_left;
+            }
+        }
+        for (int r = br1; r < br2; ++r) {
+            for (int c = bc1; c < bc2; ++c) --ss->blocked[r][c];
+        }
     }
     for (int r = r1; r < r2; ++r) ss->rows[r] += width;
     for (int c = c1; c < c2; ++c) ss->cols[c] += height;
     return (void*)(long)cnt;
 }
 
-int place_first(struct SolverState *ss)
+static int place_first(struct SolverState *ss)
 {
-    const int max_threads = 2*16*16;
-    struct ThreadState *thread_state[max_threads];
-    pthread_t thread_id[max_threads];
+    struct ThreadState *thread_state[2*HEIGHT*WIDTH];
+    pthread_t thread_id[2*HEIGHT*WIDTH];
     int num_threads = 0;
 
     for (int dir = 0; dir < 2; ++dir) {
         const int height = 1 + (lengths[0] - 1)*dir;
         const int width  = 1 + (lengths[0] - 1)*(1 - dir);
 
-        for (int r1 = 0; r1 <= 16 - height; ++r1) {
-            for (int c1 = 0; c1 <= 16 - width; ++c1) {
+        for (int r1 = 0; r1 <= HEIGHT - height; ++r1) {
+            for (int c1 = 0; c1 <= WIDTH - width; ++c1) {
                 const int r2 = r1 + height;
                 const int c2 = c1 + width;
-                for (int r = r1; r < r2; ++r) if (ss->rows[r] < width) goto next;
-                for (int c = c1; c < c2; ++c) if (ss->cols[c] < height) goto next;
+                bool valid = true;
                 for (int r = r1; r < r2; ++r) {
-                    for (int c = c1; c < c2; ++c) if (ss->blocked[r][c]) goto next;
+                    if (ss->rows[r] < width) valid = false;
+                }
+                for (int c = c1; c < c2; ++c) {
+                    if (ss->cols[c] < height) valid = false;
+                }
+                for (int r = r1; r < r2; ++r) {
+                    for (int c = c1; c < c2; ++c) {
+                        if (ss->blocked[r][c]) valid = false;
+                    }
                 }
 
-                // Allocate thread state (+1000 to prevent false sharing)
-                struct ThreadState *ts = malloc(sizeof(struct ThreadState) + 1000);
-                memset(ts, 0, sizeof(struct ThreadState));
-                memcpy(&ts->ss, ss, sizeof(struct SolverState));
-                ts->r1 = r1;
-                ts->c1 = c1;
-                ts->dir = dir;
-                thread_state[num_threads] = ts;
-                int error = pthread_create(&thread_id[num_threads], NULL, &place_first_thread, ts);
-                assert(error == 0);
-                num_threads++;
-            next: continue;
+                if (valid) {
+                    // Allocate thread state (+1000 to prevent false sharing)
+                    struct ThreadState *ts = malloc(sizeof(struct ThreadState) + 1000);
+                    memset(ts, 0, sizeof(struct ThreadState));
+                    memcpy(&ts->ss, ss, sizeof(struct SolverState));
+                    ts->r1 = r1;
+                    ts->c1 = c1;
+                    ts->dir = dir;
+                    thread_state[num_threads] = ts;
+                    if (pthread_create(&thread_id[num_threads], NULL, &place_first_thread, ts) != 0)
+                        fatal("could not spawn a new thread");
+                    num_threads++;
+                }
             }
         }
     }
-
-    printf("%d threads\n", num_threads);
 
     int total = 0;
     for (int i = 0; i < num_threads; ++i)
     {
         void *res;
-        int error = pthread_join(thread_id[i], &res);
-        assert(error == 0);
+        if (pthread_join(thread_id[i], &res) != 0)
+            fatal("could not join previously spawned thread");
         total += (int)(long)res;
-        for (int r = 0; r < 16; ++r) {
-            for (int c = 0; c < 16; ++c) {
-                ss->hits[r][c] += thread_state[i]->ss.hits[r][c];
+        for (int r = 0; r < HEIGHT; ++r) {
+            for (int c = 0; c < WIDTH; ++c) {
+                ss->hit_counts[r][c] += thread_state[i]->ss.hit_counts[r][c];
             }
         }
         free(thread_state[i]);
@@ -215,44 +250,90 @@ int place_first(struct SolverState *ss)
     return total;
 }
 
-int main()
+static void parse_counts(int *counts, int size, char *data)
 {
-    struct SolverState ss = {
-        /* 42 */
-        // { 2,1,2,2,2,4,0,1,1,2,1,5,0,1,3,3 },
-        // { 3,3,2,1,1,1,5,2,2,1,4,1,1,1,1,1 }
+    int i = 0, total = 0;
+    for (char *s = strtok(data, "."); s != NULL; s = strtok(NULL, ".")) {
+        if (i >= size) fatal("too many ship counts");
+        int val = atoi(s);
+        if (val < 0) fatal("negative ship count");
+        counts[i++] = val;
+        total += val;
+    }
+    if (i < size) fatal("too few ship counts");
+    if (total != SHIP_PARTS) fatal("total ship count is wrong");
+}
 
-        /* 194073 */
-        // { 2,4,0,0,5,0,4,0,0,5,0,0,7,0,0,3 }
-        // { 1,2,2,2,2,2,2,3,2,2,2,2,1,2,2,1 },
+int main(int argc, char *argv[])
+{
+    /* Grab command line arguments: */
+    bool arg_verbose = false;
+    char *arg_rows, *arg_cols, *arg_shots;
+    if (argc == 4 && argv[1][0] != '-') {
+        arg_verbose = false, arg_rows = argv[1], arg_cols = argv[2], arg_shots = argv[3];
+    } else if (argc == 5 && strcmp(argv[1], "-v") == 0) {
+        arg_verbose = true, arg_rows = argv[2], arg_cols = argv[3], arg_shots = argv[4];
+    } else {
+        printf("usage: [-v] %s <Rows> <Cols> <Shots>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-        /* 6346 */
-        // { 3,2,0,3,2,2,0,3,0,0,5,0,0,4,0,6 },
-        // { 1,1,1,1,2,3,3,1,2,2,4,3,2,2,1,1 }
-
-        /* 956448 (3.9s) */
-        // { 0,2,0,4,0,5,0,0,4,2,0,3,0,5,0,5 },
-        // { 1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1 }
-
-        /* 677548 (4.3s) */
-        { 2,0,4,0,3,0,5,0,5,0,4,0,2,5,0,0 },
-        { 1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1 }
-
-        /* 1,562,616 (5.4s) */
-        // { 5,0,5,0,4,0,2,0,2,0,5,0,3,0,4,0 },
-        // { 1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1 }
-    };
-
-    int cnt = place_first(&ss);
-    printf("cnt=%d\n", cnt);
-    if (cnt > 0) {
-
-        for (int r = 0; r < 16; ++r) {
-            for (int c = 0; c < 16; ++c) {
-                printf("%4.2f ", 1.0*ss.hits[r][c]/cnt);
-            }
-            printf("\n");
+    /* Construct initial solver state corresponding with arguments: */
+    struct SolverState ss;
+    memset(&ss, 0, sizeof(ss));
+    parse_counts(ss.rows, HEIGHT, arg_rows);
+    parse_counts(ss.cols, WIDTH, arg_cols);
+    for (char *s = strtok(arg_shots, "."); s != NULL; s = strtok(NULL, ".")) {
+        bool hit = s[0] == 'S';
+        if (!hit && s[0] != 'W') continue;
+        int col = s[1] - 'A';
+        if (col < 0 || col >= 16) continue;
+        int row = atoi(&s[2]) - 1;
+        if (row < 0 || row >= 16) continue;
+        if (ss.required[row][col] || ss.blocked[row][col]) fatal("duplicate shots");
+        if (hit) {
+            if (ss.required_left == SHIP_PARTS) fatal("too many hits");
+            ++ss.required[row][col];
+            ++ss.required_left;
+        } else {
+            ++ss.blocked[row][col];
         }
     }
-    /* TODO: select a maximal square to fire at */
+
+    /* Solve! */
+    int solutions = place_first(&ss);
+    if (solutions == 0) fatal("no solutions found");
+
+    /* Figure out firing options using a greedy heuristic: */
+    struct Coords options[HEIGHT*WIDTH];
+    int num_options = 0, max_hits = 0;
+    for (int r = 0; r < HEIGHT; ++r) {
+        for (int c = 0; c < WIDTH; ++c) {
+            if (ss.hit_counts[r][c] > max_hits) {
+                max_hits = ss.hit_counts[r][c];
+                num_options = 0;
+            }
+            if (ss.hit_counts[r][c] == max_hits) {
+                options[num_options].r = r;
+                options[num_options].c = c;
+                ++num_options;
+            }
+        }
+    }
+    if (num_options == 0) fatal("no cells to fire at");
+
+    if (arg_verbose) {
+        /* Print solution count and all possible options: */
+        printf("%d %d %d", solutions, max_hits, num_options);
+        for (int n = 0; n < num_options; ++n) {
+            printf(" %c%d", 'A' + options[n].r, options[n].c + 1);
+        }
+        printf("\n");
+    } else {
+        /* Print a random option: */
+        srand((unsigned)time(NULL) ^ ((unsigned)getpid() << 16));
+        struct Coords coords = options[rand()%num_options];
+        printf("%c%d\n", 'A' + coords.r, coords.c + 1);
+    }
+    return EXIT_SUCCESS;
 }
