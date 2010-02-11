354aed5b57f0939e654908214c0804860f70ad2a
Maks Verver <maksverver@geocities.com>
2010-02-12 00:44:30 +0100

    - Implemented serving ships from a pool.  - Added two pools: one with hard data (used by default) that is suitable for    optimal (brute-forcing) players, and one with medium hard data, optimized to    trick non-bruteforcing players that try to apply statistics and logic.

--- server.go
+++ server.go
@@ -2,35 +2,48 @@ package main
 
 import (
 	"exec"
-	"http"
 	"flag"
+	"http"
 	"fmt"
 	"io"
 	"log"
 	"malloc"
+	"os"
+	"rand"
 	"strings"
 	"time"
 )
 
-var gSolverPath string
+var gSolverPath string  // path to solver executable
+var gShipPool []string  // pool of ships to serve
 
-func doShips() (string, bool) { return "TODO", false }
+func doShips() (string, bool) {
+	return gShipPool[rand.Intn(len(gShipPool))], true
+}
 
-func doFire(rows string, cols string, shots string) (response string, ok bool) {
+func doFire(rows, cols, shots string) (response string, ok bool) {
 	if cmd, err := exec.Run(gSolverPath, []string{gSolverPath, rows, cols, shots},
 		[]string{}, exec.DevNull, exec.Pipe, exec.PassThrough); err != nil {
 		response = "could not run solver: " + err.String()
 	} else {
-		defer cmd.Wait(0)
-		buf := make([]byte, 1024)
-		if size, err := cmd.Stdout.Read(buf); err != nil {
+		for err == nil {
+			var buf [1024]byte
+			var len int
+			len, err = cmd.Stdout.Read(&buf)
+			response += string(buf[0:len])
+		}
+		if err != os.EOF || len(response) == 0 {
 			response = "could not read from solver: " + err.String()
 		} else {
-			response = strings.TrimSpace(string(buf[0:size]))
 			ok = true
 		}
+		cmd.Close()
 	}
 	return
+}
+
+func doFinished(ships, shots, opponent, player string) (response string, ok bool) {
+	return "", ok
 }
 
 func PlayerServer(conn *http.Conn, request *http.Request) {
@@ -58,41 +71,87 @@ func PlayerServer(conn *http.Conn, request *http.Request) {
 				response, succeeded = doFire(rows[0], cols[0], shots[0])
 			}
 		case "Finished":
-			succeeded = true
+			if ships, ok := request.Form["Ships"]; !ok {
+				response = "no Ships parameter supplied"
+			} else if shots, ok := request.Form["Shots"]; !ok {
+				response = "no Shots parameter supplied"
+			} else if opponent, ok := request.Form["Opponent"]; !ok {
+				response = "no Opponent parameter supplied"
+			} else if player, ok := request.Form["Player"]; !ok {
+				response = "no Player parameter supplied"
+			} else {
+				response, succeeded = doFinished(ships[0], shots[0], opponent[0], player[0])
+			}
 		default:
 			response = "unknown Action value supplied"
 		}
 	}
 
-	// Log request details:
-	{
-		var parameters string
-		for name, value := range (request.Form) {
-			if len(name) > 0 {
-				if parameters != "" {
-					parameters += " "
-				}
-				parameters += (name + "=" + value[0])
-			}
-		}
-		delay := fmt.Sprintf("\t%.3fs", float64(time.Nanoseconds()-nsBegin)/1e9)
-		log.Stdout(
-			"\t"+conn.RemoteAddr,
-			"\t"+parameters,
-			"\t"+fmt.Sprintf("%v", succeeded),
-			"\t"+response,
-			"\t"+delay)
-	}
-
 	// Write response to client:
 	conn.SetHeader("Content-Type", "text/plain")
-	if !succeeded {
-		response = "ERROR: " + response + "!\n"
+	if succeeded {
+		io.WriteString(conn, response)
+	} else {
+		io.WriteString(conn, "ERROR: " + response + "!\n")
 	}
-	io.WriteString(conn, response)
+
+	// Log request details:
+	var parameters string
+	for name, value := range (request.Form) {
+		if len(name) > 0 {
+			if parameters != "" {
+				parameters += " "
+			}
+			parameters += (name + "=" + value[0])
+		}
+	}
+	delay := fmt.Sprintf("\t%.3fs", float64(time.Nanoseconds()-nsBegin)/1e9)
+	log.Stdout(
+		"\t"+conn.RemoteAddr,
+		"\t"+parameters,
+		"\t"+fmt.Sprintf("%v", succeeded),
+		"\t"+response,
+		"\t"+delay)
 
 	// Run GC, because it doesn't seem to happen automatically (Go bug?)
 	malloc.GC()
+}
+
+func readFile(name string) (string, os.Error) {
+	var res string
+	file, err := os.Open(name, os.O_RDONLY, 0)
+	for err == nil {
+		var buf [1024]byte
+		var len int
+		len, err = file.Read(&buf)
+		res += string(buf[0:len])
+	}
+	if err != os.EOF {
+		return "", err
+	}
+	return res, nil
+}
+
+func readShipPool(name string) []string {
+	data, err := readFile(name)
+	if err != nil || len(data) == 0 {
+		log.Stderr("Could not read pool data from `" + name + "'")
+		return nil
+	}
+	lines := strings.Split(data, "\n", 0)
+	for len(lines) > 0 && len(lines[len(lines)-1]) == 0 {
+		lines = lines[0:len(lines)-1]
+	}
+	return lines
+}
+
+func findSolverPath(name string) string {
+	path, err := exec.LookPath(name)
+	if err != nil {
+		log.Stderr("Could not find solver executable `" + name + "': " + err.String())
+		return ""
+	}
+	return path
 }
 
 func main() {
@@ -101,14 +160,14 @@ func main() {
 	port := flag.Int("p", 14000, "port to bind")
 	rootPath := flag.String("r", "/player", "root path for player")
 	solverPath := flag.String("s", "./solver", "path to solver")
+	poolPath := flag.String("i", "pool-hard.txt", "path to ships pool")
 	flag.Parse()
 
-	// Find solver binary location:
-	if path, err := exec.LookPath(*solverPath); err != nil {
-		log.Stderr("Could not find solver executable `" + *solverPath + "': " + err.String())
-	} else {
-		gSolverPath = path
-
+	// Initialize global data:
+	rand.Seed(time.Nanoseconds())
+	gShipPool = readShipPool(*poolPath)
+	gSolverPath = findSolverPath(*solverPath)
+	if gShipPool != nil && gSolverPath != "" {
 		// Start an HTTP server with a player handler:
 		addr := fmt.Sprintf("%s:%d", *host, *port)
 		http.Handle(*rootPath, http.HandlerFunc(PlayerServer))
